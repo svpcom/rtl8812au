ad2eb2e5fc5c7af9870ce3a4945df92557fe6e33
Vasily Evseenko <svpcom@p2ptech.org>
2022-04-26 00:59:09 +0300

    Add compat with kernel > 5.12 #14

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -586,18 +586,16 @@ static const struct ieee80211_txrx_stypes
 
 static u64 rtw_get_systime_us(void)
 {
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39))
-	struct timespec ts;
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 20, 0))
-	getboottime(&ts);
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39) && LINUX_VERSION_CODE < KERNEL_VERSION(4, 20, 0))
+        struct timespec ts;
+        get_monotonic_boottime(&ts);
+        return ((u64)ts.tv_sec * 1000000) + ts.tv_nsec / 1000;
+#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 20, 0))
+        return ktime_to_us(ktime_get_boottime());
 #else
-	get_monotonic_boottime(&ts);
-#endif
-	return ((u64)ts.tv_sec * 1000000) + ts.tv_nsec / 1000;
-#else
-	struct timeval tv;
-	do_gettimeofday(&tv);
-	return ((u64)tv.tv_sec * 1000000) + tv.tv_usec;
+        struct timeval tv;
+        do_gettimeofday(&tv);
+        return ((u64)tv.tv_sec * 1000000) + tv.tv_usec;
 #endif
 }
 
