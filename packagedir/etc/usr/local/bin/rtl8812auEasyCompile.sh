9b48ac7d056c60f612c65b17dae70445e308a664
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-04 00:42:27 +0100

    create install service

---
+++ packagedir/etc/usr/local/bin/rtl8812auEasyCompile.sh
@@ -0,0 +1,7 @@
+echo "We're now compiling the rtl8812au driver into your stock kernel"
+sudo apt install dkms
+./opt/rtl8812au/dkms-install.sh
+systemctl disable rtl8812auEasyCompile
+sudo rm -Rf /etc/systemd/system/rtl8812auEasyCompile.service
+sudo rm -Rf /usr/local/bin/rtl8812auEasyCompile.sh
+echo "cleanup done"
