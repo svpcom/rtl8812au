c26125f4e4ac25373d0175b89173d18dc166da48
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-04 02:20:15 +0100

    add shebang line

--- packagedir/usr/local/bin/rtl8812auEasyCompile.sh
+++ packagedir/usr/local/bin/rtl8812auEasyCompile.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 echo "We're now compiling the rtl8812au driver into your stock kernel"
-sudo apt install dkms
+sudo apt install -y dkms
 ./opt/rtl8812au/dkms-install.sh
 systemctl disable rtl8812auEasyCompile
 sudo rm -Rf /etc/systemd/system/rtl8812auEasyCompile.service
