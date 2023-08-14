cd39349f319693eeb8ffc9a5f491d55b68a32b9d
consti10 <geierconstantinabc@gmail.com>
2023-08-15 01:29:55 +0200

    document

--- include/rtw_xmit.h
+++ include/rtw_xmit.h
@@ -77,6 +77,7 @@
 /* xmit extension buff defination */
 #define MAX_XMIT_EXTBUF_SZ	(1536)
 
+// OpenHD: This is the (monitor mode) frame(s) tx queue size in the driver
 #ifdef CONFIG_SINGLE_XMIT_BUF
 	#define NR_XMIT_EXTBUFF	(1)
 #else
