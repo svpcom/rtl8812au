e9154cf62fe6ec655c38b65049f5cc35cb58f50d
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-12-30 20:51:22 +0100

    testing versioning

--- include/rtw_version.h
+++ include/rtw_version.h
@@ -1,1 +1,12 @@
-#define DRIVERVERSION	"v5.2.20.2_28373.20190919"
+#ifndef RTW_VERSION_H
+#define RTW_VERSION_H
+
+// Helper macro to convert a macro to a string
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+
+// Generate the version string with the current date
+#define CURRENT_DATE __DATE__
+#define DRIVERVERSION "v5.2.20.2_ohd_" STR(CURRENT_DATE)
+
+#endif // RTW_VERSION_H
