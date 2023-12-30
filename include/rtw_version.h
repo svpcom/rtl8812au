705d5600ea0da80c408583e6d7e4791bf8c11395
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-12-30 21:42:59 +0100

    alphanumerical

--- include/rtw_version.h
+++ include/rtw_version.h
@@ -5,8 +5,8 @@
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
 
-// Generate the version string with the current date
+// Generate the version string with the current date in the format v5.2.20.2_OHD_20122023
 #define CURRENT_DATE __DATE__
-#define DRIVERVERSION "v5.2.20.2_ohd_" STR(CURRENT_DATE)
+#define DRIVERVERSION "v5.2.20.2_OHD_" STR(CURRENT_DATE[4]) STR(CURRENT_DATE[5]) STR(CURRENT_DATE[7]) STR(CURRENT_DATE[8]) STR(CURRENT_DATE[9]) STR(CURRENT_DATE[22]) STR(CURRENT_DATE[23])
 
 #endif // RTW_VERSION_H
