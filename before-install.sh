05b801c43ace351b9e306266d2832cda557f1cb8
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-01-10 01:33:10 +0100

    Update before-install.sh

--- before-install.sh
+++ before-install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo rm -Rf /lib/modules/6.3.13-060313-generic/kernel/drivers/net/wireless/88XXau_wfb.ko
+if [ -e "/lib/modules/6.3.13-060313-generic/kernel/drivers/net/wireless/88XXau_wfb.ko" ]; then
+    sudo rm -Rf "/lib/modules/6.3.13-060313-generic/kernel/drivers/net/wireless/88XXau_wfb.ko"
+fi
+if [ -e "/lib/modules/6.3.13-060313-generic/kernel/drivers/net/wireless/88XXau_ohd.ko" ]; then
+    sudo rm -Rf "/lib/modules/6.3.13-060313-generic/kernel/drivers/net/wireless/88XXau_ohd.ko"
+fi
